c0f209d524b84866a5f6abd2cccca174ed7eeb5b
shuying <mailtoshuying@gmail.com>
2018-04-11 18:56:02 +0800

    add new service: frpsrv

---
+++ frpsrv/buildorker.sh
@@ -0,0 +1,2 @@
+docker build -t vpcdata/frpsrv:1.0 ./
+docker tag vpcdata/frpsrv:1.0 vpcdata/frpsrv:latest
---
+++ frpsrv/firewall.sh
@@ -0,0 +1,2 @@
+firewall-cmd --permanent --add-port=60000-62000/tcp
+firewall-cmd --permanent --add-port=60000-62000/tcp
